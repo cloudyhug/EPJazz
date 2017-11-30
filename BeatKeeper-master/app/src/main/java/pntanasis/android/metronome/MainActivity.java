0557127664fdbc68d92286bd7ec45be5e6b0c2be
maxime <maxime.le-feuvre@insa-rennes.fr>
2017-12-01 00:15:58 +0100

    Création d'une page permettant de changer les paramètres du metronome (seul le layout est fait) puis de lancer le metronome

--- BeatKeeper-master/app/src/main/java/pntanasis/android/metronome/MainActivity.java
+++ BeatKeeper-master/app/src/main/java/pntanasis/android/metronome/MainActivity.java
@@ -3,18 +3,31 @@ package pntanasis.android.metronome;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TabHost;
 
 /**
  * Created by maxime on 23/11/2017.
  */
 
 public class MainActivity extends Activity {
+    TabHost tabHost;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.parametrage);
+
+
+
+
     }
+
+
+
 
     public void page1(View view){
         startActivity(new Intent(this,MetronomeActivity.class));
